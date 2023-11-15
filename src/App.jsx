9721102269e2fdc07517cc0463d659575a715016
YutoSuzuki <113349377+suzuyu0115@users.noreply.github.com>
2023-11-15 23:47:13 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { ColoredMessage } from "./components/ColoredMessage";
+
+export const App = () => {
+  console.log("レンダリング");
+
+  const [num, setNum] = useState(0);
+
+  const onClickButton = () => {
+    alert();
+    setNum(num + 1);
+  };
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
+      <ColoredMessage color="pink">元気です！</ColoredMessage>
+      <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
+    </>
+  );
+};
